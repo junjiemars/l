f2e6ac497ba40f02dbd7296503a83926944f16ad
南山竹 <junjiemars@gmail.com>
2016-11-07 23:34:25 +0800

    tcp server

---
+++ src/tcp/server.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require racket/tcp)
+
+(define (server port)
+  (define listener (tcp-listen port 5 #t))
+  (define (loop)
+    (on-accept listener)
+    (loop))
+  (loop))
+
+(define (on-accept listener)
+  (define-values (in out) (tcp-accept listener))
+  (handle in out)
+  (close-input-port in)
+  (close-output-port out))
+
+(define (handle in out)
+  ; Discard the request header (up to blank line):
+  (regexp-match #rx"(\r\n|^)\r\n" in)
+  ; Send reply:
+  (display "HTTP/1.0 200 Okay\r\n" out)
+  (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
+  (display "<html><body>Hello, world!</body></html>" out))
+
