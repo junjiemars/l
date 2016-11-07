562d1d971fbc13771837c7b0fb2d65c01570e831
南山竹 <junjiemars@gmail.com>
2016-11-07 14:31:07 +0800

    refine lang/symbol.rkt

--- src/lang/symbol.rkt
+++ src/lang/symbol.rkt
@@ -1,10 +1,16 @@
 #lang racket
 
+;; all #t
 (eq? 'abc 'abc)
+(eq? 'abc `abc)
+(eq? 'abc (quote abc))
+
+;; first #t others #f
 (eq? 'abc (string->symbol "abc"))
 (eq? 'abc (string->uninterned-symbol "abc"))
 (eq? 'abc (string->unreadable-symbol "abc"))
 
+;; first and third #t, second #f
 (symbol-interned? 'abc)
 (symbol-interned? (string->unreadable-symbol "abc"))
 (symbol-unreadable? (string->unreadable-symbol "abc"))
