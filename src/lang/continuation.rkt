387acba04081a58a2639697258d886c8e5022106
南山竹 <junjiemars@gmail.com>
2016-11-28 17:54:09 +0800

    accumulator via continuation

--- src/lang/continuation.rkt
+++ src/lang/continuation.rkt
@@ -16,4 +16,25 @@
 ;; saved-k like saved-k-fn
 (define (saved-k-fn v)
   (+ 1 (+ 2 (+ 3 (+ v 4 5)))))
+(= 35 (saved-k-fn 20))
+
+
+(define accumulator #f)
+(define (accumulate!)
+  (let ([i 0])
+    (call-with-composable-continuation
+     (λ (x) (set! accumulator x)))
+    (set! i (add1 i))
+    i))
+
+#|
+(accumulate!) ;; 1
+(accumulator) ;; 2
+(accumulator) ;; 3
+
+(define previous-accumulator accumulator)
+(accumulate!) ;; 1
+(accumulator) ;; 2
+(previous-accumulator) ;; 4
+|#
 
