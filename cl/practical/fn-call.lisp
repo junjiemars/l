7f312cd3b47b50f73d01b3cdaffce312ace5fd87
南山竹 <junjiemars@gmail.com>
2016-12-10 22:48:37 +0800

    fn call

---
+++ cl/practical/fn-call.lisp
@@ -0,0 +1,18 @@
+
+(defun sqr (n) (* n n))
+
+(eq (function sqr) #'sqr)
+(eq #'sqr (symbol-function 'sqr))
+
+(funcall #'sqr 3)
+
+(defun plot (fn min max step)
+  (loop for i from min to max by step do
+       (loop repeat (funcall fn i) do
+            (format t "*"))
+       (format t "~%")))
+
+((lambda (x) (* x x)) 3)
+
+(defvar x1 (lambda (x) (* 2 x)))
+(funcall x1 3)
