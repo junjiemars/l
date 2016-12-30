52b5cf9e6e3ea11c708fcf6e97875633175c03c8
南山竹 <junjiemars@gmail.com>
2016-12-30 17:56:09 +0800

    cool, elastic api will be created via macros

--- cl/elastic/kit.lisp
+++ cl/elastic/kit.lisp
@@ -4,14 +4,13 @@
 
 (setf drakma:*header-stream* *standard-output*)
 
-;; (import 'http-request :drakma)
-;; (import 'flexi-external-formt :flexi-streams)
+
 
 (defmacro lookup (url &key
                         ((:method m) :get)
                         ((:parameters a) nil has-a)
                         ((:content c) nil has-c) 
-                        ((:encode e) #'j:encode-json)
+                        ((:encode e) #'j:encode-json-to-string)
                         ((:decode d) #'j:decode-json))
   `(let ((s (w:http-request ,url
                             :method ,m
@@ -22,9 +21,32 @@
      (funcall ,d s)))
 
 
+(defmacro make-url-base (&key (host "172.17.0.2") (port 9200))
+  `(with-output-to-string (s)
+     (format s "http://~a:~a" ,host ,port)))
+
+
 (defmacro make-url (base &key (index nil) (type nil) (id nil))
   `(with-output-to-string (s)
      (format s "~a" ,base)
      (dolist (x (list ,index ,type ,id))
        (when x (format s "/~a" x)))))
+                 
+
+(defmacro make-match (out &optional (field nil) (value nil))
+  `(let* ((all-p (and (null ,field) (null ,value)))
+          (m (if all-p 'match_all 'match)))
+     (j:with-object (,out)
+       (j:as-object-member (m ,out)
+         (j:with-object (,out)
+           (if (not all-p)
+               (j:encode-object-member ,field ,value ,out)))))))
+             
+(defmacro make-query (&optional (query 'make-match) &rest args)
+  `(with-output-to-string (s)
+     (j:with-object (s)
+       (j:as-object-member ('query s)
+         (,query s ,@args)))))
+
+
                  
