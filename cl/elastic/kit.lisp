0b3b155c33fe6cda6035800bbd6681a73fe33d19
南山竹 <junjiemars@gmail.com>
2016-12-30 15:34:28 +0800

    make-url for elastic api

--- cl/elastic/kit.lisp
+++ cl/elastic/kit.lisp
@@ -7,25 +7,24 @@
 ;; (import 'http-request :drakma)
 ;; (import 'flexi-external-formt :flexi-streams)
 
-(defun lookup (url &key ((:content c) nil has-c) ((:parameters a) nil has-a)
-                     ((:method m) :get)
-                     ((:encode e) 'j:encode-json-alist-to-string)
-                     ((:decode d) 'j:decode-json))
-  (let ((s (w:http-request url
-                           :method m
-                           :want-stream t
-                           :content (when has-c (funcall e c))
-                           :parameters (when has-a a))))
-    (setf (fs:flexi-stream-external-format s) :utf-8)
-    (funcall d s)))
-
-
-(defun add (url d)
-  (let* ((j (j:encode-json-alist-to-string d))
-         (s (w:http-request url
+(defmacro lookup (url &key
+                        ((:method m) :get)
+                        ((:parameters a) nil has-a)
+                        ((:content c) nil has-c) 
+                        ((:encode e) #'j:encode-json)
+                        ((:decode d) #'j:decode-json))
+  `(let ((s (w:http-request ,url
+                            :method ,m
                             :want-stream t
-                            :content j)))
-    (setf (fs:flexi-stream-external-format s) :utf-8)
-    (j:decode-json s)))
+                            :content (when ,has-c (funcall ,e ,c))
+                            :parameters (when ,has-a ,a))))
+     (setf (fs:flexi-stream-external-format s) :utf-8)
+     (funcall ,d s)))
 
 
+(defmacro make-url (base &key (index nil) (type nil) (id nil))
+  `(with-output-to-string (s)
+     (format s "~a" ,base)
+     (dolist (x (list ,index ,type ,id))
+       (when x (format s "/~a" x)))))
+                 
