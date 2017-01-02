d064c93f86f0b4ac1fbf2fcab34f48c88995df66
南山 <junjiemars@gmail.com>
2017-01-02 12:45:34 +0800

    :cl environment variables, command-line arguments

---
+++ cl/lang/env.lisp
@@ -0,0 +1,30 @@
+
+(defun env-checking ()
+  (list
+   (short-site-name)
+   (long-site-name)
+   (lisp-implementation-type)
+   (lisp-implementation-version)
+   (machine-instance)
+   (machine-type)
+   (machine-version)
+   (software-type)
+   (software-type)
+   (software-version)))
+
+;; set/unset env variables
+(sb-posix:getenv "NJINA")
+(sb-posix:setenv "NINJA" "Jakawa" 1)
+(string= "Jakawa" (sb-posix:getenv "NJINA"))
+(sb-posix:unsetenv "NINJA")
+
+;; command-line arguments
+sb-ext:*posix-argv*
+
+;; create a executable: a Lisp Image
+;; run sbcl from console
+#|
+(defun foo ()
+  (format t "Hello, world!~%The time is ~A~%" (get-universal-time)))
+(sb-ext:save-lisp-and-die #p"foo.exe" :toplevel #'hello :executable t)
+|#
