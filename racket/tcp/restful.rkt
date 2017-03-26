98e4a7fd518855785bff9d3cacb6a585e4b1ee88
南山竹 <junjiemars@gmail.com>
2017-03-26 19:49:25 +0800

    http-client

--- racket/tcp/restful.rkt
+++ racket/tcp/restful.rkt
@@ -1,7 +1,13 @@
 #lang racket
 
-(require request)
+(require net/http-client)
 
-(define check-self-ip
-  (make-domain-requester "checkip.dns.he.net" http-requester))
 
+;(define check-self-ip
+;  (make-domain-requester "checkip.dns.he.net" http-requester))
+
+(define (check-self-ip)
+  (let ((conn (http-conn-open "checkip.dns.he.net")))
+    (display (http-conn-recv! conn
+                              #:close? true))))
+  
