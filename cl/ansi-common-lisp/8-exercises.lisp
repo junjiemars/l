aeb80f4b9439f3760edea0f0426b4af10b940204
南山竹 <junjiemars@gmail.com>
2017-07-03 22:18:52 +0800

    ansi common lisp: 8 symbols > exercises

--- cl/ansi-common-lisp/8-exercises.lisp
+++ cl/ansi-common-lisp/8-exercises.lisp
@@ -12,5 +12,9 @@
   (:nicknames "B")
   (:export "aaa"))
 
+;; Q1: Is it possible for two symbols to have the same name but not be eql?
+(and (string= (symbol-name 'a:|aaa|)
+              (symbol-name 'b:|aaa|))
+     (not (eql 'a:|aaa| 'b:|aaa|)))
 
-(eql 'a:|aaa| 'b:|aaa|)
+;; Q2: 
