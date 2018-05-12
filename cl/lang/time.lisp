6faddae5a63b27fa515809d458eccd025f310a8f
南山竹 <junjiemars@gmail.com>
2018-05-12 20:58:15 +0800

    refine time.lisp

--- cl/lang/time.lisp
+++ cl/lang/time.lisp
@@ -6,34 +6,35 @@
 
 ;; unix time since 00:00:01 on January 1,1970 UTC aka POSIX time or Epoch time
 ;; and not include leap seconds.
+;; unix timestamp: date +%s
 
 
 ;; day-of-week start from Monday(0) end to Sunday(6)
 ;; ~ in (format) constrol string at the end of a line, and ~, newline,
 ;; whitespace following the newline are removed from the output.
 
-(defun format-decoded-time ()
+(defun format-universal-time (&optional universal-time time-zone)
   (multiple-value-bind
         (second minute hour date month year day-of-week daylight-p zone)
-      (get-decoded-time)
-    (format t
-            "second:~a~%~
-             minute:~a~%~
-             hour:~a~%~
-             date:~a~%~
-             month:~a~%~
-             year:~a~%~
-             day-of-week:~a~%~
-             daylight-p:~a~%~
-             zone:~a~%"
-            second minute hour date month year day-of-week daylight-p zone)))
+      (if universal-time
+					(decode-universal-time universal-time time-zone)
+					(get-decoded-time))
+		(list :second second
+					:minute minute
+					:hour hour
+					:date date
+					:month month
+					:year year
+					:day-of-week day-of-week
+					:daylight-p daylight-p
+					:zone zone)))
 
 (decode-universal-time (get-universal-time))
 (encode-universal-time 01 1 13 2 1 17 -8)
 (get-decoded-time)
 
 
-;; (ql:quickload :local-time)
+(ql:quickload :local-time)
 (local-time:now)
 (local-time:encode-timestamp 123456789 0 10 12 23 12 1965)
 (local-time:parse-timestring "1965-12-23")
