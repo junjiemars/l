ab001c11b7f22f7a07ad802b50239f314c225f7e
南山竹 <junjiemars@gmail.com>
2018-05-15 21:32:54 +0800

    1.11 exercises

---
+++ cl/paip/1.11-exercises.lisp
@@ -0,0 +1,34 @@
+;;;; -*- Mode: Lisp; Syntax: Common-Lisp -*-
+
+
+;; 1.2 Write a function to exponentiate, or raise a number to an integer
+;; power.
+
+(defun power (x n)
+	"Power raises X to the nth power, N must be an integer >= 0.
+This executes in log N time, because of the check for even N."
+	(cond ((= n 0) 1)
+				((evenp n) (expt (power x (/ n 2)) 2))
+				(t (* x (power x (- n 1))))))
+
+;; linear iteration
+(defun power1 (x n)
+	(if (= n 0)
+			1
+			(* x (power1 x (1- n)))))
+
+;; linear tail recursion
+(defun power2 (x n)
+	(labels ((iter (x n p)
+						 (if (= n 0)
+								 p
+								 (iter x (1- n) (* x p)))))
+		(iter x n 1)))
+
+;; log N recursion
+(defun power3 (x n)
+	(labels ((square (x)
+						 (* x x)))
+		(cond ((= n 0) 1)
+					((evenp n) (square (power3 x (/ n 2))))
+					(t (* x (power3 x (1- n)))))))
