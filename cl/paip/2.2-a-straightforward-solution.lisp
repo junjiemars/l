9e6bf86c86f2a1684aaf97f47c49b0340413955d
南山竹 <junjiemars@gmail.com>
2018-05-16 00:08:18 +0800

    2.2 adj* prep*

--- cl/paip/2.2-a-straightforward-solution.lisp
+++ cl/paip/2.2-a-straightforward-solution.lisp
@@ -10,11 +10,24 @@
 
 
 (defun sentence () (append (noun-phrase) (verb-phrase)))
-(defun noun-phrase () (append (Article) (Noun)))
+(defun noun-phrase () (append (Article) (Adj*) (Noun) (PP*)))
 (defun verb-phrase () (append (Verb) (noun-phrase)))
 (defun Article () (one-of '(the a)))
 (defun Noun () (one-of '(man ball woman table)))
 (defun Verb () (one-of '(hit took saw liked)))
+(defun Adj () (one-of '(big little blue green adiabatic)))
+(defun PP () (append (Prep) (noun-phrase)))
+(defun Prep () (one-of '(to in by with on)))
+
+(defun Adj* ()
+	(if (= (random 2) 0)
+			nil
+			(append (Adj) (Adj*))))
+
+(defun PP* ()
+	(if (random-elt '(t nil))
+			(append (PP) (PP*))
+			nil))
 
 (sentence)
 (noun-phrase)
