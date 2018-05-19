e3e34593e5bcc350c008b6afe172409aca5c614d
南山竹 <junjiemars@gmail.com>
2018-05-19 22:59:02 +0800

    1.4 lists

---
+++ paip/1.4-lists.lisp
@@ -0,0 +1,5 @@
+;;;; -*- Mode: Lisp; Syntax: Common-Lisp -*-
+
+;; The symbol nil and form () are completely synonymous; they are both
+;; representabtions of the empty list. nil is also used to denote the "false"
+;; value in Lisp.
