8f5336676cc0011b2c9b0b0180cb17ea117fcf33
南山竹 <junjiemars@gmail.com>
2018-05-20 21:47:29 +0800

    1.8 other data types

---
+++ paip/1.8-other-data-types.lisp
@@ -0,0 +1,25 @@
+;;;; -*- Mode: Lisp; Syntax: Common-Lisp -*-
+
+#|
+Lisp objects: 
+numbers
+symbols
+lists
+functions
+vectors
+arrays
+structures
+characters
+stream
+hash tables
+and others
+|#
+
+"a string" ;; => "a string"
+(length "a string") ;; => 8
+(length "") ;; => 0
+
+
+
+
+
