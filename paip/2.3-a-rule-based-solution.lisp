84591e7870db3987127798907baa65c3484136df
南山竹 <junjiemars@gmail.com>
2018-05-21 22:49:04 +0800

    exercise: 2.1, 2.2

--- paip/2.3-a-rule-based-solution.lisp
+++ paip/2.3-a-rule-based-solution.lisp
@@ -84,7 +84,46 @@ original program.
 ;; Exercise 2.1
 ;; Write a version of generate that uses cond but avoids calling rewrites twice.
 
+(defun generate2 (phrase)
+	(let ((choices nil))
+		(cond ((listp phrase)
+					 (mappend #'generate2 phrase))
+					((setf choices (rewrites phrase))
+					 (generate (random-elt choices)))
+					(t (list phrase)))))
+
 
 ;; Exercise 2.2
 ;; Write a version of generate that explicitly differentiates between
 ;; terminal symbols (those with no rewrite rules) and nonterminal symbols.
+
+(defun non-terminal-p (category)
+	(not (null (rewrites category))))
+
+(defun generate3 (phrase)
+	(cond ((listp phrase))
+				((non-terminal-p phrase)
+				 (generate (random-elt (rewrites phrase))))
+				(t (list phrase))))
+
+;;;;
+;; 2.5-changing-the-grammar-without-changing-the-program.lisp
+;;;;
+
+(defparameter *bigger-grammar*
+	'((sentence -> (noun-phrase verb-phrase))
+		(noun-phrase -> (article adj* noun pp*) (name) (pronoun))
+		(verb-phrase -> (verb noun-phrase))
+		(pp* -> () (pp pp*))
+		(adj* -> () (adj adj*))
+		(pp -> (prep noun-phrase))
+		(prep -> to in by with on)
+		(adj -> big little blue green adiabatic)
+		(article -> the a)
+		(name -> Pat Kim Lee Terry Robin)
+		(noun -> man ball woman table)
+		(verb -> hit took saw liked)
+		(pronoun -> he she it these those that))
+	"A grammar for a trivial subset of English.")
+
+(setf *grammar* *bigger-grammar*)
---
+++ paip/2.5-changing-the-grammar-without-changing-the-program.lisp
@@ -0,0 +1,4 @@
+;;;; -*- Mode: Lisp; Syntax: Common-Lisp --*
+
+;; in "2.3-a-rule-based-solution.lisp"
+
