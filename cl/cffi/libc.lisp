56e00aace0bd3bb47b7f830a73a9aeac1381e71c
南山竹 <junjiemars@gmail.com>
2020-05-03 19:25:31 +0800

    libc

---
+++ cl/cffi/libc.lisp
@@ -0,0 +1,58 @@
+;;;; -*- Mode: lisp;
+
+(defpackage #:cffi-libc
+  (:use #:cl #:cffi)
+  (:export
+   #:cffi-version
+   #:getenv
+   #:setenv
+   #:gethostname
+   #:gettimeofday))
+
+(in-package #:cffi-libc)
+
+(defun cffi-version ()
+  (asdf:component-version (asdf:find-system 'cffi)))
+
+(defcfun "getenv" :string
+  (name :string))
+
+(defcfun "setenv" :int
+  (name :string)
+  (value :string)
+  (overwrite :int))
+
+(defcfun ("gethostname" %gethostname) :int
+  (name :pointer)
+  (namelen :int))
+
+(defun gethostname ()
+  (with-foreign-pointer-as-string ((name namelen) 255)
+    (%gethostname name namelen)))
+
+(defcstruct timeval
+  (tv-sec :long)
+  (tv-usec :long))
+
+(define-foreign-type null-pointer-type ()
+  ()
+  (:actual-type :pointer)
+  (:simple-parser null-pointer))
+
+(defmethod translate-to-foreign (value (type null-pointer-type))
+  (cond
+    ((null value) (null-pointer))
+    ((null-pointer-p value) value)
+    (t (error "~A is not a null pointer." value))))
+
+(defcfun ("gettimeofday" %gettimeofday) :int
+  (tp :pointer)
+  (tzp null-pointer))
+
+(defun gettimeofday ()
+  (with-foreign-object (tv 'timeval)
+    (%gettimeofday tv nil)
+    (with-foreign-slots ((tv-sec tv-usec) tv timeval)
+      (values tv-sec tv-usec))))
+
+
