03aba40bc2e76d74336e6666ada8ef27527be443
南山竹 <junjiemars@gmail.com>
2020-06-25 12:03:11 +0800

    8.2-property-lists

--- ansi-common-lisp/8.2-property-lists.lisp
+++ ansi-common-lisp/8.2-property-lists.lisp
@@ -1,5 +1,7 @@
+;; In CL every symbol has a property-list, or `plist'
 
 (get 'alizarin 'color)
+(get 'alizarin 'color "green")
 
 (setf (get 'alizarin 'color) 'red)
 
@@ -8,3 +10,15 @@
 (setf (get 'alizarin 'transparency) 'high)
 
 (symbol-plist 'alizarin)
+
+;; package -> name       -> "FOO"
+;;         -> package    -> hosting
+;;         -> value      0x11223344
+;;         -> function   #<function>
+;;         -> plist      (color red)
+
+;; Notice that property-list are not represented as assoc-lists,
+;; though they are used the same way.  It uses `eql' to compare keys.
+;; 
+;; In CL, property-list aren't used very much. They have largely been
+;; superseded by hash tables.
