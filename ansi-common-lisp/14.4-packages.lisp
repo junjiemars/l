88a8adbf3782f3a7d4de14aefd123c874347d6c8
南山竹 <junjiemars@gmail.com>
2020-06-25 11:25:18 +0800

    14.4-packages

---
+++ ansi-common-lisp/14.4-packages.lisp
@@ -0,0 +1,24 @@
+(package-name *package*)
+
+(find-package "COMMON-LISP-USER")
+
+;; Usually a symbol is interned in the package that was current at the time
+;; it was read.thy
+(symbol-package 'sym)
+(symbol-package 'gensym)
+
+;; Reading the expression caused `sym' to be interned.
+(setf sym 99)
+
+(make-package 'XXX
+              :use '(common-lisp))
+;; using `slime' change package to XXX
+;; inspect the `sym' symbol
+;; common-lisp-user::sym
+
+(export 'sym)
+(common-lisp-user:sym)
+(unintern 'sym)
+
+(import 'common-lisp-user:sym)
+(use-package 'common-lisp-user)
