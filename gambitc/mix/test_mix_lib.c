a55df30e6ad1dce467acf9c056031e93079664cb
南山竹 <junjiemars@gmail.com>
2020-09-21 21:37:52 +0800

    gambitc/mix: shared library

---
+++ gambitc/mix/test_mix_lib.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define _unused_(x) ((void)(x))
+
+extern int power_of_2(int);
+extern 
+
+int
+main(int argc, char **argv) {
+  _unused_(argc);
+  _unused_(argv);
+
+  int n = 0;
+  if (argc > 1) {
+    sscanf(argv[1], "%i", &n);
+  }
+  
+  printf("power_of_2(%i) = %i\n", n, power_of_2(n));
+
+  return 0;
+}
