e1d72060c8a47a9a6018f63e6a9d5c98fb50634e
南山竹 <junjiemars@gmail.com>
2020-09-26 16:01:51 +0800

    gambitc/c: c-declare

---
+++ gambitc/c/b2.scm
@@ -0,0 +1,28 @@
+;;; 
+;;; make clean test
+;;; 
+
+(c-declare #<<c-declare-end
+
+#include <stdio.h>
+char *welcome (const char *name);
+
+char *
+welcome (const char *name) {
+  static char ss [64];
+  snprintf (ss, 63, "Welcome %s", name);
+  return ss;
+}
+
+
+c-declare-end
+)
+
+
+(define welcome
+  (c-lambda (char-string)
+            char-string
+            "welcome"))
+
+
+(println (welcome "ZZZ"))
