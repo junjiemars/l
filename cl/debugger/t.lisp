665c403a6d09bd5948132db0f61d4cc8f6e484f8
南山竹 <junjiemars@gmail.com>
2021-05-15 21:10:14 +0800

    Hofstadter sequence

---
+++ cl/debugger/t.lisp
@@ -0,0 +1,15 @@
+;;; Hofstadter sequence
+
+;;; F(0) = 1
+;;; M(0) = 0
+;;; F(n) = n - M(F(n-1)), n > 0
+;;; M(n) = n - F(M(n-1)), n > 0
+
+
+(defun female (n)
+	(cond ((zerop n) 1)
+				(t (- n (male (female (1- n)))))))
+
+(defun male (n)
+	(cond ((zerop n) 0)
+				(t (- n (female (male (1- n)))))))
